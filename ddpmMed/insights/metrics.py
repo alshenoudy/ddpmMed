dead537f55f70d1d041238cf9532231c35f22268
Ahmed AlShenoudy <ahmed.alshenoudy@gmail.com>
2022-03-28 23:00:48 +0200

    additions to brats experiment

--- ddpmMed/insights/metrics.py
+++ ddpmMed/insights/metrics.py
@@ -1,7 +1,3 @@
-import os
-from types import Union
-from typing import Any
-
 import torch
 import numpy as np
 import SimpleITK as sitk
@@ -11,30 +7,15 @@ import matplotlib.pyplot as plt
 class SegmentationMetrics:
     def __init__(self):
         self.overlap = sitk.LabelOverlapMeasuresImageFilter()
-        self.all_metrics = [
-            self.jaccard_coefficient,
-            self.dice_coefficient,
-            self.hausdorff_distance,
-            self.false_negatives,
-            self.false_positives
-        ]
 
-    def _convert_to_sitk_image(self, image: Union[sitk.Image, np.ndarray, torch.Tensor]):
-        raise NotImplementedError
+    def calculate_metrics(self, prediction, image):
 
-    def jaccard_coefficient(self, prediction: Any, ground_truth: Any):
-        raise NotImplementedError
-
-    def dice_coefficient(self, prediction: Any, ground_truth: Any):
-        raise NotImplementedError
-
-    def false_negatives(self, prediction: Any, ground_truth: Any):
-        raise NotImplementedError
-
-    def false_positives(self, prediction: Any, ground_truth: Any):
-        raise NotImplementedError
-
-    def hausdorff_distance(self, prediction: Any, ground_truth: Any):
-        raise NotImplementedError
-
-
+        self.overlap.Execute(sitk.GetImageFromArray(prediction),
+                             sitk.GetImageFromArray(image))
+        metrics = {
+            'jaccard': self.overlap.GetJaccardCoefficient(),
+            'dice': self.overlap.GetDiceCoefficient(),
+            'fp_error': self.overlap.GetFalsePositiveError(),
+            'fn_error': self.overlap.GetFalseNegativeError()
+        }
+        return metrics
